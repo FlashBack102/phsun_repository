f6794a583c92182e4c6fffbb7d3b16389d29a8bc
phsun102 <mks33077@naver.com>
2020-12-08 16:31:08 +0900

    오렌지 수정

--- orange.java
+++ orange.java
@@ -9,13 +9,14 @@ public class BootStartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootStartApplication.class, args);
 	}
-<<<<<<< HEAD
 
 	public String test() {
 		int a = 3;
 		return "";
 	}
 
-=======
->>>>>>> 61909c2e9fa7025eb8d9bc0431ecf7f688cd8a72
+	public int test2() {
+		int cnt = 100;
+		return cnt;
+	}
 }
