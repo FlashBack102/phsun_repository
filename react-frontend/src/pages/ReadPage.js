cb7ae9dbb0383fca119885c3b8fd43187646cebb
phsun102 <mks33077@naver.com>
2021-01-01 01:20:02 +0900

    route

---
+++ react-frontend/src/pages/ReadPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReadPage = () => {
+  return <div>읽기</div>;
+};
+
+export default ReadPage;
---
+++ react-frontend/src/pages/WritePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WritePage = () => {
+  return <div>작성</div>;
+};
+
+export default WritePage;
